05e9a298f9f11d6bbf018547448a67a114cff436
Pablo Hidalgo <pabloracer_1992@hotmail.com>
2017-11-08 15:02:21 -0300

    first commit

---
+++ example1-Xamarin/example1-Xamarin/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace example1_Xamarin
+{
+    [Activity(Label = "example1_Xamarin", MainLauncher = true)]
+    public class MainActivity : app
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
